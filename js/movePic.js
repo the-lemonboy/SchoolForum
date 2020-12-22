56c72c1852d77095db144311f05956afd9b799f0
the-lemonboy <lingzhengpeng@outlook.com>
2020-12-22 21:12:20 +0800

    items

---
+++ js/movePic.js
@@ -0,0 +1,74 @@
+﻿var picArr = document.getElementsByClassName("picArr");
+var nextBtn = document.getElementById("nextBtn");
+var prevBtn = document.getElementById("prevBtn");
+var navIcon = document.getElementsByClassName("navicon");
+var index = 0;
+//初始化图片的类名
+var iniClass = function(){
+    for(var i=0; i<picArr.length; i++){
+        picArr[i].className = "picArr";
+    }
+}
+//改变后的图片类名
+var changeClass = function(){
+    iniClass();
+    picArr[index].className = "picArr Active";
+}
+//方向键控制图片
+function imgChange(){
+  //点击右方向键图片变下一张
+    nextBtn.addEventListener("click",function(){
+        if(index<picArr.length-1){
+            index++; 
+        }
+      else{
+        index=0;
+      }
+      changeClass();
+    });
+    //  //点击右方向键图片变上一张
+    prevBtn.addEventListener("click",function(){
+      
+        if(index>0){
+          
+            index--;
+           
+        }
+      else{
+        index=picArr.length-1;
+      }
+    changeClass();
+    })
+}
+//加一个定时器，图片自动切换
+imgChange();
+function autoMove(){
+   setInterval(function(){
+    if(index<picArr.length-1){
+        index++;       
+    }
+  else{
+    index=0;
+  }
+  changeClass();    
+   },3000);  
+}
+autoMove();
+//导航栏
+ var nav = function(){ 
+  if(index<picArr.length-1){
+        index++;       
+    }
+  else{
+    index=0;
+  } 
+  navIcon[index].style.backgroundColor = "black";
+
+} 
+function navMove(){
+  autoMove(); 
+   nav();
+}
+navMove();
+    
+
---
+++ js/musicJS.js
@@ -0,0 +1,84 @@
+﻿const prevbtn = document.getElementById("prevbtn");
+const nexttbtn = document.getElementById("nextbtn");
+const music = document.getElementById("music");
+const pausebtn = document.getElementById("pausebtn");
+var song1 = document.getElementById("song1");
+var song2 = document.getElementById("song2");
+var song3 = document.getElementById("song3");
+var song4 = document.getElementById("song4");
+//获取歌单
+var audios =new Array("../audio/lyall.mp3", "../audio/oww.mp3");
+var i = 0;
+//播放暂停
+var play = false;
+function pausefun(){
+    pausebtn.addEventListener("click", function(){
+        //判断当前是否在播放
+        play = !play;
+        
+     if (play){
+        pausebtn.innerHTML = '<img src="../images/childImages/pause1.png">';
+         music.play(); 
+     }else{
+        pausebtn.innerHTML = '<img src="../images/childImages/pause2.png">';
+         music.pause();
+     }
+    
+    });
+}
+//下一首
+ function nextfun(){
+    nextbtn.addEventListener("click", function(){
+        
+        if(i==audios.length-1){
+            alert("当前为最后一首!");
+        }
+        else{
+            pausebtn.innerHTML = '<img src="../images/childImages/pause1.png">';
+            i++;
+            music.src = audios[i];
+            music.play();
+        }
+        
+    });
+}
+//上一首
+function prevfun(){
+    prevbtn.addEventListener("click", function(){
+        if(i==0){
+            alert("当前为第一首!");
+        }
+        else{
+            pausebtn.innerHTML = '<img src="../images/childImages/pause1.png">';
+            i--;
+            music.src = audios[i];
+            music.play();
+        }
+    });
+} 
+function listSongfun(){
+    song1.addEventListener("click",function(){
+        pausebtn.innerHTML = '<img src="../images/childImages/pause1.png">';
+        music.src=audios[1];
+        music.play();
+    })
+    song2.addEventListener("click",function(){
+        pausebtn.innerHTML = '<img src="../images/childImages/pause1.png">';
+        music.src=audios[0];
+        music.play();
+    })
+    song3.addEventListener("click",function(){
+        pausebtn.innerHTML = '<img src="../images/childImages/pause1.png">';
+        music.src=audios[0];
+        music.play();
+    })
+    song4.addEventListener("click",function(){
+        pausebtn.innerHTML = '<img src="../images/childImages/pause1.png">';
+        music.src=audios[0];
+        music.play();
+    })
+}
+ pausefun();
+ nextfun();
+prevfun(); 
+listSongfun();
